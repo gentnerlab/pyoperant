a7e0a6265395df9526488af69a676939a946831b
Justin Kiggins <Justin@aves.ucsd.edu>
2012-05-22 16:41:25 -0700

    fixed README import in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author = 'Justin Kiggins',
     author_email = 'justin.kiggins@gmail.com',
     description = 'hardware interface and controls for operant conditioning in the Gentner Lab',
-    long_description = read('README.md'),
+    long_description = read('README'),
     packages = ['pyoperant'],
     scripts = ['tricks/lights.py',],
     requires = ['pyephem'],
