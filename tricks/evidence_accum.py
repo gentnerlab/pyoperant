4825b735276e9f25a392333e504b6b547b1ccefc
Justin Kiggins <justin.kiggins@gmail.com>
2013-12-13 18:00:06 -0800

    refactoring utils and evidence_accumulation code

--- tricks/evidence_accum.py
+++ tricks/evidence_accum.py
@@ -12,135 +12,153 @@ import datetime as dt
 from pyoperant import utils
 from pyoperant.components import GoodNite
 
-class GNGSeqExperiment(utils.Experiment):
+def get_options(cmd_line):
+    """ get all of the configuration options for the experiment """
+
+    # set path variables
+    options['code_path'] = os.path.dirname(os.path.realpath(__file__))
+    options['user_path'] = os.path.expanduser('~')
+    options['bird_path'] = os.path.join(USER_PATH,'opdat', 'B' + cmd_line['subj'])
+    options['stim_path'] = os.path.join(options['bird_path'],'stims')
+
+    with open(os.path.join(options['bird_path'], cmd_line['config_file'])) as config_f:
+        options = json.load(config_f)
+
+    options['box_id'] = cmd_line['box']
+    options['subject_id'] = cmd_line['subj']
+
+    options['script_fname'] = os.path.basename(__file__)
+
+    return options
+
+class EvidenceAccumExperiment(utils.Experiment):
     """docstring for Experiment"""
     def __init__(self, *args, **kwargs):
         super(Experiment, self, *args, **kwargs).__init__()
-        self.options = self.get_options()
-        self.log_config()
-        self.init_summary()
 
-    def get_stimulus(self,trial_class):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        # assign stim files full names
+        for name, filename in self.stims.items():
+            filename_full = os.path.join(self.stim_path, filename)
+            self.stims[name] = filename_full
+
+        # define log files, rDAT files
+        self.filetime_fmt = '%Y%m%d%H%M%S'
+        self.exp_timestamp = dt.datetime.now().strftime(self.filetime_fmt)
+
+        # configure logging
+        self.log_file = os.path.join(self.bird_path, self.subject_id + '.log')
+        self.log_config()
+
+        # configure csv file for data
+        self.data_csv = os.path.join(self.bird_path, self.subject_id + '_' + self.script_fname + '_trials_' + self.exp_timestamp + '.csv')
+        self.fields_to_save = ['number','type','trial_start','class','stim_start','stim_string','response','feed','timeout','cum_correct','cum_correct_thresh']
+        with open(self.data_csv, 'wb') as data_fh:
+            trialWriter = csv.writer(data_fh)
+            trialWriter.writerow(self.fields_to_save)
+
+        # # save a snapshot of the configuration
+        # # TODO: pickle this object
+        # self.config_snapshot = os.path.join(self.bird_path, self.subject_id + '_' + self.script_fname + '_config_' + timestamp + '.json')
+        # with open(self.config_snapshot, 'wb') as config_snap:
+        #     json.dump(options, config_snap, sort_keys=True, indent=4)
+
+        self.summaryDAT = os.path.join(self.bird_path,self.subject_id + '.summaryDAT')
+        self.init_summary()
+        
+
+        # run through the transitions for each stimulus and generate transition matrixes
+        n = len(self.stims)
+        self.evidence_arrays = {}
+        for stim_class, pair_set in self.model_evidence.items():
+            arr = np.zeros((n+1,n+1),np.float_)
+            for a,b in pair_set:
+                arr[a,b] = 1.0
+
+            self.evidence_arrays[stim_class] = arr
+
+        self.stim_classes = self.model_evidence.keys()
+        assert len(self.stim_classes) == 2
+        self.transition_cdf = {}
+        for this_class in self.stim_classes:
+            other_class = [cl for cl in self.stim_classes if cl is not this_class][0]
+            trans = self.odds * self.evidence_arrays[stim_class] + self.evidence_arrays[other_class]
+            trans[0,1:] = 1.0
+            trans = np.cumsum(trans,axis=1)
+            trans = trans / trans[:,-1]
+            self.transition_cdf[this_class] = trans
+
+    def get_stimuli(self,trial_class):
         """ take trial class and return a tuple containing the wav filename & additional info to play 
 
         take in the trial class and the options dict
         returns a stimulus dictionary
 
         """
-        # determine stimulus for next trial, based on class
-        stim = dict()
-        stim['class'] = trial_class
+        from pyoperant.utils import AuditoryStimulus, Event
 
-        stim['string'] = []
-        input_file_list = []
-        trial_isi = []
-        motif_id = 0
-        cum_matrix = np.cumsum(self.options['models'][trial_class], axis=1)
-        for item in range(self.options['strlen_max']):
-            motif_id = (cum_matrix[motif_id] < random.random()).sum()
-            motif_name = self.options['stim_map'][str(motif_id)]
-            isi = random.gauss(self.options['isi_mean'], options['isi_stdev'])
-            if isi < 0.0: isi = 0.0
-            input_file_list.append((self.options['stims'][motif_name], isi))
-            stim['string'].append(motif_name)
-        input_file_list[-1] = (input_file_list[-1][0], 0.0)
-        stim['input_file_list'] = input_file_list
+        input_files = []
+        motif_ids = []
 
-        (stim['filename'], stim['epochs']) = utils.concat_wav(stim['input_file_list'], os.path.join(self.options['stim_path'], 'temp_concat.wav'))
+        # use transition CDF to get iteratively get next motif id
+        mid = 0
+        for pos in range(self.strlen_max):
+            mid = (self.transition_cdf[trial_class][mid] < random.random()).sum()
+            motif_ids.append(mid)
 
-        stim['dur'] = stim['epochs'][-1][-1]
+        assert len(motif_ids) == self.strlen_max
+
+        motifs = [self.stim_map[str(mid)] for mid in motif_ids]
+
+        motif_isi = [max(random.gauss(self.isi_mean, self.isi_stdev),0.0) for mot in motifs]
+        motif_isi[-1] = 0.0
+
+        input_files = [(self.stims[motif_name], isi) for motif_name, isi in zip(motifs,motif_isi)]
+        filename =  os.path.join(self.stim_path, ''.join(motifs) + '.wav')
+        stim, epochs = utils.concat_wav(input_files,filename)
       
-        return stim
+        return stim, epochs
 
     def save_trial(self,trial_dict):
         # write trial results to CSV
-        with open(self.options['data_csv'],'ab') as data_fh:
-            trialWriter = csv.DictWriter(data_fh,fieldnames=self.options['fields_to_save'],extrasaction='ignore')
+        with open(self.data_csv,'ab') as data_fh:
+            trialWriter = csv.DictWriter(data_fh,fieldnames=self.fields_to_save,extrasaction='ignore')
             trialWriter.writerow(trial_dict)
 
-    def get_options(self):
-        """ get all of the configuration options for the experiment """
 
-        cmd_line = utils.parse_commandline())
+# def run_trial(trial, options):
+#     pass
 
-        # set path variables
-        CODE_PATH = os.path.dirname(os.path.realpath(__file__))
-        USER_PATH = os.path.expanduser('~')
-        BIRD_PATH = os.path.join(USER_PATH,'opdat', 'B' + cmd_line['subj'])
-        STIM_PATH = os.path.join(BIRD_PATH,'stims')
-
-        with open(os.path.join(BIRD_PATH, cmd_line['config_file'])) as config_f:
-            options = json.load(config_f)
-        options['box_id'] = cmd_line['box']
-        options['subject_id'] = cmd_line['subj']
-        options['code_path'] = CODE_PATH
-        options['user_path'] = USER_PATH
-        options['bird_path'] = BIRD_PATH
-        options['stim_path'] = STIM_PATH
-        options['script_fname'] = os.path.basename(__file__)
-
-        # run through stim files
-        for name, filename in options['stims'].items():
-            filename_full = os.path.join(options['stim_path'], filename)
-            options['stims'][name] = filename_full
-
-        # define log files, rDAT files
-        filetime_fmt = '%Y%m%d%H%M%S'
-        timestamp = dt.datetime.now().strftime(filetime_fmt)
-
-        options['log_file'] = os.path.join(options['bird_path'], options['subject_id'] + '.log')
-        options['data_csv'] = os.path.join(options['bird_path'], options['subject_id'] + '_' + options['script_fname'] + '_trials_' + timestamp + '.csv')
-        options['config_snapshot'] = os.path.join(options['bird_path'], options['subject_id'] + '_' + options['script_fname'] + '_config_' + timestamp + '.json')
-        options['summaryDAT'] = os.path.join(options['bird_path'], options['subject_id'] + '.summaryDAT')
-        options['fields_to_save'] = ['number','type','trial_start','class','stim_start','stim_string','response','feed','timeout','cum_correct','cum_correct_thresh']
+# class VRTrial(utils.Event):
+#     """docstring for Trial"""
+#     def __init__(self, correct=False,cum_correct=0,cum_correct_thresh=1, *args, **kwargs):
+#         super(Trial, self, *args, **kwargs).__init__()
         
-        # oreo
-        with open(options['data_csv'], 'wb') as data_fh:
-            trialWriter = csv.writer(data_fh)
-            trialWriter.writerow(options['fields_to_save'])
-        with open(options['config_snapshot'], 'wb') as config_snap:
-            json.dump(options, config_snap, sort_keys=True, indent=4)
 
-        # run through the transitions for each stimulus and generate transition matrixes
-        n = len(options['stims'])
-        options['models'] = {}
-        for stim_class, pair_set in options['diagnostic_transitions'].items():
-            mat = np.zeros((n+1,n+1))
-            mat[0][1:] = 1.0/n
-            for pair in pair_set:
-                mat[pair[0]][1:] = (1-options['alpha'])/(n-1)
-                mat[pair[0]][pair[1]] = options['alpha']
-            options['models'][stim_class] = mat
+def main(options):
 
-        return options
-
-def run_trial(trial, options):
-    pass
-
-if __name__ is "__main__":
-
-
-    experiment = GNGSeqExperiment()
+    experiment = EvidenceAccumExperiment(**options)
 
     # initialize box
-    box = hwio.OperantBox(options['box_id'])
-    box.house_light.schedule = experiment.options['light_schedule']
+    box = hwio.OperantBox(experiment.box_id)
+    box.house_light.schedule = experiment.light_schedule
     box.reset()
     
-    experiment.log.debug('box %i initialized' % experiment.options['box_id'])
+    experiment.log.debug('box %i initialized' % experiment.box_id)
     
-    trial = dict()
-    trial['correct'] = False
-    trial['cum_correct'] = 0
-    trial['cum_correct_thresh'] = 1
-    trial['number'] = 0
+    trial = {
+        'correct': False,
+        'cum_correct': 0,
+        'cum_correct_thresh': 1,
+        'number': 0,
+        }
 
     do_correction = False
 
     # start experiment
-    do_experiment = True
-    while do_experiment:
-        
+    while True:
         try:
             # make sure lights are on at the beginning of each trial, prep for trial
             box.house_light.set_by_schedule()
@@ -149,12 +167,12 @@ if __name__ is "__main__":
             trial['timeout'] = False
             trial['number'] += 1
 
-            if do_correction and options['correction_trials']:
+            if do_correction and experiment.correction_trials:
                 trial['type'] = 'correction'
                 experiment.log.debug("correction trial: class is %s" % trial['class'])
             else:
                 trial['type'] = 'normal'
-                trial['class'] = random.choice(options['models'].keys())
+                trial['class'] = random.choice(experiment.models.keys())
                 trial_stim = experiment.get_stimulus(trial['class'])
                 experiment.log.debug("trial class is %s" % trial['class'])
             
@@ -173,11 +191,11 @@ if __name__ is "__main__":
             box.center.off()
 
             # wait for response
-            min_wait = trial_stim['epochs'][options['strlen_min']-1][-1]
+            min_wait = trial_stim['epochs'][experiment.strlen_min]-1][-1]
             utils.wait(min_wait)
 
             # check for responses
-            max_wait = trial_stim['epochs'][-1][-1] + options['response_win']
+            max_wait = trial_stim['epochs'][-1][-1] + experiment.response_win
             check_peck = True
             box.left.on()
             box.right.on()
@@ -223,7 +241,7 @@ if __name__ is "__main__":
                     if trial['type'] is not 'correction':
                         trial['cum_correct'] += 1 
                     
-                    if options['secondary_reinf']:
+                    if experiment.secondary_reinf:
                         # give secondary reinforcer
                         trial['flash_epoch'] = box.center.flash(dur=0.5)
 
@@ -233,28 +251,28 @@ if __name__ is "__main__":
                         trial['feed'] = True
 
                         try:
-                            trial['feed_epoch'] = box.reward(value=experiment.options['feed_dur'][trial['class']])
+                            trial['feed_epoch'] = box.reward(value=experiment.feed_dur][trial['class']])
                         
                         # but catch the feed errors
                         except hwio.ResponseDuringFeedError as err:
                             trial['feed'] = 'Error'
                             experiment.summary['responses_during_feed'] += 1
                             experiment.log.error("response during feed on box %s" % str(err))
-                            utils.wait(options['feed_dur'][trial['class']])
+                            utils.wait(experiment.feed_dur[trial['class']])
                             box.reset()
 
                         except hwio.HopperAlreadyUpError as err:
                             trial['feed'] = 'Error'
                             experiment.summary['hopper_already_up'] += 1
                             experiment.log.warning("hopper already up on box %s" % str(err))
-                            utils.wait(options['feed_dur'][trial['class']])
+                            utils.wait(experiment.feed_dur[trial['class']])
                             box.reset()
 
                         except hwio.HopperDidntRaiseError as err:
                             trial['feed'] = 'Error'
                             experiment.summary['hopper_failures'] += 1
                             experiment.log.error("hopper didn't come up on box %s" % str(err))
-                            utils.wait(options['feed_dur'][trial['class']])
+                            utils.wait(experiment.feed_dur[trial['class']])
                             box.reset()
 
                         except hwio.HopperDidntDropError as err:
@@ -266,20 +284,20 @@ if __name__ is "__main__":
                         finally:
                             box.house_light.on()
                             if trial['type'] is not 'correction':
-                                trial['cum_correct_thresh'] = random.randint(1, 2*experiment.options['variable_ratio']-1)
+                                trial['cum_correct_thresh'] = random.randint(1, 2*experiment.variable_ratio-1)
                 
                 elif trial['response'] is 'none':
                     # ignore non-responses
                     pass
 
                 else:
-                    trial['timeout_epoch'] = box.punish(value=options['timeout_dur'][trial['class']])
+                    trial['timeout_epoch'] = box.punish(value=experiment.timeout_dur[trial['class']])
                     trial['timeout'] = True
                     trial['cum_correct'] = 0
                     do_correction = True
 
             trial['trial_end'] = dt.datetime.now()
-            experiment.save_trial(trial, options)
+            experiment.save_trial(trial)
 
             if trial['feed']:
                 trial['cum_correct'] = 0
@@ -288,7 +306,7 @@ if __name__ is "__main__":
             """ reset experimental parameters for the next day """
             poll_int = 60.0
             box.lights_off()
-            experiment.summary = utils.init_summary()
+            experiment.init_summary()
             experiment.log.debug('waiting %f seconds before checking light schedule...' % (poll_int))
             utils.wait(poll_int)
 
@@ -301,6 +319,14 @@ if __name__ is "__main__":
 
         finally:
             experiment.write_summary()
+
+
+
+if __name__ is "__main__":
+
+    options = get_options(utils.parse_commandline())
+
+    main(options)
 
 
 
