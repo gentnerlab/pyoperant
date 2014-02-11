8dfe675b9c29d415fc6af37b71c2839c4a589cb7
Gentnerlab User <justin.kiggins+gentnerlab@gmail.com>
2014-02-11 14:16:10 -0800

    syntax fixes for shape

--- pyoperant/tricks/shape.py
+++ pyoperant/tricks/shape.py
@@ -16,9 +16,9 @@ class Shaper(object):
 									peck_block=self._peck_block(),
 									response_block=self._response_block())
 
-# Block 1:  Hopper comes up on VI (stays up for 5 s) for the first day 
-# that the animal is in the apparatus. Center key flashes for 5 sec, prior 
-# to the hopper access. If the center key is pressed while flashing, then 
+# Block 1:  Hopper comes up on VI (stays up for 5 s) for the first day
+# that the animal is in the apparatus. Center key flashes for 5 sec, prior
+# to the hopper access. If the center key is pressed while flashing, then
 # the hopper comes up and then the session jumps to block 2 immediately
 
 	def _hopper_block(self, next_state):
@@ -28,12 +28,12 @@ class Shaper(object):
 									init=self._block_init(),
 									wait=self._wait_block(10, 40,'check'),
 									check=self._check_block('flash_mid', 1, float('inf')),
-									flash_mid=self._flash_poll(self.panel.mid, 5, 'reward', 'pre_reward'),
+									flash_mid=self._flash_poll(self.panel.center, 5, 'reward', 'pre_reward'),
 									pre_reward=self._pre_reward('reward'),
 									reward=self.reward(5, 'wait'))
 		return next_state
 
-# Block 2:	The center key flashes until pecked.  When pecked the hopper comes up for 
+# Block 2:	The center key flashes until pecked.  When pecked the hopper comes up for
 # 			4 sec. Run 100 trials.
 # 			reverts to revert_state if no response before timeout (60*60*3=10800)
 	def _peck_block(self, next_state, revert_state, reps=100, revert_timeout=10800):
@@ -52,12 +52,12 @@ class Shaper(object):
 			return revert_state
 
 # Block 3:	The center key flashes until pecked, then either the right or left (p = .5)
-# 			key flashes until pecked, then the hopper comes up for 3 sec. Run 100 trials. 
+# 			key flashes until pecked, then the hopper comes up for 3 sec. Run 100 trials.
 
 	def _response_block(self):
 		pass
 
-# Block 4:	Wait for peck to non-flashing center key, then right or left key flashes 
+# Block 4:	Wait for peck to non-flashing center key, then right or left key flashes
 # 			until pecked, then food for 2.5 sec.   Run 100 trials.
 
 	def _block_init(self):
@@ -91,7 +91,7 @@ class Shaper(object):
 	def _flash_poll(self, component, duration, next_state, reward_state=None):
 		utils.run_state_machine(	start_in='init',
 									init=self._polling_init('main'),
-									main=self._flashing_main(component, 5, 1))
+									main=self._flashing_main(component, duration, 1))
 		if self.responded_poll:
 			return reward_state
 		else:
@@ -121,7 +121,7 @@ class Shaper(object):
 	def _light_poll(self, component, duration, next_state, reward_state=None):
 		utils.run_state_machine(	start_in='init',
 									init=self._polling_init('main'),
-									main=self._light_main(component, 5))
+									main=self._light_main(component, duration))
 		if self.responded_poll:
 			return reward_state
 		else:
