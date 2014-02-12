ae1c3e39d6b04ad8478fb3ba3c89334286ed0bdb
Marvin T <mthielk@ucsd.edu>
2014-02-11 17:51:18 -0800

    response_block2 or block 4 added

--- pyoperant/tricks/shape.py
+++ pyoperant/tricks/shape.py
@@ -17,7 +17,8 @@ class Shaper(object):
                                     error_callback=self.error_callback,
                                     hopper_block=self._hopper_block('peck_block'),
                                     peck_block=self._peck_block('response_block','hopper_block'),
-                                    response_block=self._response_block(None, 'peck_block'),
+                                    response_block=self._response_block('response_block2', 'peck_block'),
+                                    response_block2=self._response_block2(None, 'response_block')
                                     sleep_block=self._run_sleep)
 
 # Block 1:  Hopper comes up on VI (stays up for 5 s) for the first day
@@ -94,6 +95,30 @@ class Shaper(object):
 
 # Block 4:  Wait for peck to non-flashing center key, then right or left key flashes
 #           until pecked, then food for 2.5 sec.   Run 100 trials.
+
+    def _response_block2(self, next_state, revert_state, reps=100, revert_timeout=10800):
+        def temp():
+            self.recent_state = 'response_block'
+            utils.run_state_machine(    start_in='init',
+                                        error_state='check',
+                                        error_callback=self.error_callback,
+                                        init=self._block_init('check'),
+                                        check=self._check_block('poll_mid', reps, revert_timeout),
+                                        poll_mid=self._poll(self.panel.center, 10, 'check', 'coin_flip'),
+                                        coin_flip=self._coin_flip('check_right', 'check_left', p=.5),
+                                        check_right=self._check_block('poll_right', reps, revert_timeout),
+                                        poll_right=self._flash_poll(self.panel.right, 10, 'check_right', 'pre_reward'),
+                                        check_left=self._check_block('poll_left', reps, revert_timeout),
+                                        poll_left=self.flash_poll(self.panel.left, 10, 'check_left', 'pre_reward'),
+                                        pre_reward=self._pre_reward('reward'),
+                                        reward=self.reward(2.5))
+            if not utils.check_time(self.parameters['light_schedule']):
+                return 'sleep_block'
+            if self.responded_block:
+                return next_state
+            else:
+                return revert_state
+        return temp
 
     def _block_init(self, next_state):
         def temp():
@@ -193,6 +218,28 @@ class Shaper(object):
                 return 'main'
             else:
                 component.off()
+                return None
+        return temp
+
+    def _poll(self, component, duration, next_state, reward_state=None):
+        def temp():
+            utils.run_state_machine(    start_in='init',
+                                        init=self._polling_init('main'),
+                                        main=self._light_main(component, duration))
+            if self.responded_poll:
+                return reward_state
+            else:
+                return next_state
+
+    def _poll_main(self, component, duration):
+        def temp():
+            elapsed_time = (dt.datetime.now() - self.polling_start).total_seconds()
+            if elapsed_time <= duration:
+                if component.status():
+                    self.responded_poll = True
+                    return None
+                return 'main'
+            else:
                 return None
         return temp
 
