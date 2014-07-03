06df9508e27408921f9e0f2cf8d3b625d9df2590
Justin Kiggins <justin.kiggins@gmail.com>
2014-07-03 14:40:38 -0700

    added in mocking of modules

--- docs/conf.py
+++ docs/conf.py
@@ -263,3 +263,30 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
+import sys
+
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['comedi','comedi_poll', 'numpy', 'ephem']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
