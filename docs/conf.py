a275d52ef219dd49c87796caf2412a165a7b2470
Justin Kiggins <justin.kiggins@gmail.com>
2014-07-03 14:21:57 -0700

    try newer version of sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.viewcode',
     # 'sphinx.ext.autosummary',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
