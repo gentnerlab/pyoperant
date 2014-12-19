c87cf3527d93a21edcd7124d8936e8791ebff2f8
Justin Kiggins <justin.kiggins@gmail.com>
2014-12-19 11:32:33 -0800

    prevents setting VR or FR < 1. fixes #34

--- pyoperant/reinf.py
+++ pyoperant/reinf.py
@@ -57,7 +57,7 @@ class FixedRatioSchedule(BaseSchedule):
     """
     def __init__(self, ratio=1):
         super(FixedRatioSchedule, self).__init__()
-        self.ratio = ratio
+        self.ratio = min(ratio,1)
         self._update()
 
     def _update(self):
