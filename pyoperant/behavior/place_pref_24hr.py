ef15a8a156663cb5c29e17d0734ab64453fedc88
Jeffrey Xing <j8xing@ucsd.edu>
2024-08-19 15:54:54 -0700

    Update 24hr for testing

--- pyoperant/behavior/place_pref_24hr.py
+++ pyoperant/behavior/place_pref_24hr.py
@@ -49,7 +49,7 @@ class PlacePrefExp24hr(base.BaseExp):
     '''
 
     def __init__(self, *args, **kwargs):
-        super(PlacePrefExp, self).__init__(*args, **kwargs)
+        super(PlacePrefExp24hr, self).__init__(*args, **kwargs)
 
         ## assign stim files full names
         for name, filename in self.parameters['stims_A'].items():
@@ -92,17 +92,20 @@ class PlacePrefExp24hr(base.BaseExp):
             day = self.parameters['experiment_start_day']
         )
 
+        self.daylight = None
+
         self.current_perch = {'IR': None, 'IRName': None, 'speaker': None}
         self.current_visit = None
         self.stimulus_event = None
 
-        self.reinforcement_counter = {'L': None, 'R': None, 'C': None} ## set up separate reinforcement counters for all perches
+        self.reinforcement_counter = {'L': None, 'R': None, 'C': None}  ## set up separate reinforcement counters for all perches
 
         self.arduino = serial.Serial(self.parameters['arduino_address'], self.parameters['arduino_baud_rate'], timeout = 1)
         self.arduino.reset_input_buffer()
 
         self.data_csv = os.path.join(self.parameters['experiment_path'],
                                      self.parameters['subject']+'_trialdata_'+self.timestamp+'.csv')
+
         self.make_data_csv()
 
     def make_data_csv(self):
@@ -115,7 +118,7 @@ class PlacePrefExp24hr(base.BaseExp):
             trialWriter = csv.writer(data_fh)
             trialWriter.writerow(self.fields_to_save)
 
-    def open_all_perches(self):
+def open_all_perches(self):
         """
         At down state (no active trials), open IR on all perches
         """
@@ -125,8 +128,8 @@ class PlacePrefExp24hr(base.BaseExp):
         ## If one of them is broken, return true
         while self.current_perch['IR'] == None:
 
-            ## check if time of day is appropriate for a visit
-            if self.check_session_schedule() == False:
+            ## check if a daylight cycle shift should occur
+            if self.check_session_schedule() :
                 return
 
             ## for each iteration, check for perching behavior.
@@ -173,314 +176,37 @@ class PlacePrefExp24hr(base.BaseExp):
         else:
             self.log.debug("Perch valid at %s" % (self.current_perch))
 
-    def current_perch_stim_class(self):
+    def light_switch():
+        '''
+        This checks if paradigm daylight is incongruent with system time
+        '''
+        return self.daylight != utils.check_time(self.parameters['light_schedule'])
+
+    def session_pre(self):
         """
-        Look in self.parameters for the order of randomized perch orders
-        depending on what day of experiment we are on
-        Can be "S" for silence, "A" for category A, "B" for category B
-        """
-
-        return self.parameters['perch_sequence'][str(self.experiment_day())][self.current_perch['speaker']]
-
-    def current_variable_ratio(self):
-        """
-        Look in self.parameters for current reinforcement ratio of the day
-        """
-
-        return self.parameters['perch_sequence'][str(self.experiment_day())][3] ## index 3 is always the variable ratio
-
-    def experiment_day(self):
-        """
-        Figure out what day of experiment we are on.
-        """
-        return (dt.date.today() - self.exp_strt_date).days
-
-
-    def validate_perching(self):
-        """
-        For any perching behavior to be valid,
-        the IR needs to be broken for at least one second.
-        """
-        ## validate current perch
-        self.log.debug("Trying to validate perch...")
-
-        ## for each perch validation effort, the beam-break has a certain amount of graces
-        ##
-        grace_tokens = self.parameters['grace_num']
-
-        while True:
-            elapsed_time = (dt.datetime.now() - self.current_visit.perch_strt).total_seconds()
-            ## keep asking if the current perch is still broken
-            if (self.current_perch['IR'].status() == True):
-                ## if elapsed time is more than a minimum perch time, perching is valid
-                if elapsed_time > self.parameters['min_perch_time']:
-                    return True
-                ## if time is not up, continue the loop
-                else:
-                    continue
-            ## if current perch is no longer broken, record perch end time, give a grace period
-            else:
-                self.current_visit.perch_end = dt.datetime.now()
-                ## if there is no grace_tokens left, immediately terminate visit
-                if grace_tokens <= 0:
-                    self.log.debug("Perching not valid. Out of grace tokens.")
-                    return False
-
-                ## while the current perch is unperched,
-                while (self.current_perch["IR"].status() == False):
-                    ## if the grace period has ended
-                    grace_period = (dt.datetime.now() - self.current_visit.perch_end).total_seconds()
-                    if grace_period > self.parameters['perch_grace_period']:
-                        self.log.debug("Perching not valid. Exceed grace period.")
-                        return False
-                    else:
-                        grace_tokens = grace_tokens - 1
-                        continue
-
-    def validate_deperching(self):
-        """
-        For any deperching behavior to be valid,
-        the IR needs to be unbroken for at least one second.
-        """
-        grace_tokens = self.parameters['grace_num']
-
-        while True:
-            ## keep asking if the current perch is still broken
-            if (self.current_perch['IR'].status() == True):
-                ## if the IR is still broken, no deperch
-                return False
-            ## if the current perch is no longer broken, record perch_end time, and give a grace period
-            else:
-                self.current_visit.perch_end = dt.datetime.now()
-                ## if there is no grace_tokens left, immediately terminate visit
-                if grace_tokens <= 0:
-                    self.log.debug("Perching Unstable. Out of grace tokens.")
-                    return True
-
-                ## while the current perch is unperched,
-                while (self.current_perch['IR'].status() == False):
-                    ## if the grace period has ended, bird has deperched
-                    grace_period = (dt.datetime.now() - self.current_visit.perch_end).total_seconds()
-                    if grace_period > self.parameters['perch_grace_period']:
-                        self.log.debug("Subject Deperched. Exceed grace period.")
-                        return True
-                    else:
-                        grace_tokens = grace_tokens - 1
-                        continue
-
-    def switch_speaker(self):
-        """
-        Use serial communication with the connected Arduino to switch
-        """
-        self.log.debug("Switching speaker relay to %s" % self.current_perch['speaker'])
-        self.arduino.write(str(self.current_perch['speaker'] + 1).encode('utf-8'))
-
-    def reinforcement_logic(self):
-        """
-        Figure out if current trial should be reinforced based on
-        reinforcement schedule of the day and previous reinforcement
+        Check if session should start in daylight or night.
+        If daylight, state machine will transition to day loop in session_main.
+        If night, state machine will transition to night loop in session_post.
         """
 
-        if self.reinforcement_counter[self.current_visit.perch_loc] == None:
-            ## start new reinforcement_counter
-            self.log.info("Reinforcement empty at current perch. Calling in reinforcement at ratio %s." % str(self.current_variable_ratio()))
-            self.reinforcement_counter[self.current_visit.perch_loc] = random.randint(1, 2*self.current_variable_ratio() - 1) - 1
-            self.log.info("Reinforcement inbound in %s visits." % str(self.reinforcement_counter[self.current_visit.perch_loc]))
+        ## assign light schedule to
+        self.daylight = utils.check_time(self.parameters['light_schedule'])
 
-        ## If there are reinforcement counter is 0, reinforce
-        if self.reinforcement_counter[self.current_visit.perch_loc] == 0:
-            self.log.info("Reinforcement available at current perch. Reinforcing...")
-            self.reinforcement_counter[self.current_visit.perch_loc] = None ## wipe reinforcement
-            return True
+        if self.daylight:
+            return "main"
         else:
-            self.log.info("Reinforcement not available at current perch, inbound in %s visits. " % str(self.reinforcement_counter[self.current_visit.perch_loc]))
-            self.reinforcement_counter[self.current_visit.perch_loc] = self.reinforcement_counter[self.current_visit.perch_loc] - 1
-            return False
+            return "post"
 
-    def stimulus_shuffle(self):
-        """
-        While perched, shuffle stimuli from a library
-        """
-
-        ## decide if reinforcement should be given
-        self.current_visit.reinforcement = self.reinforcement_logic()
-
-        ## if the current class is silence, or if reinforcement is not given, don't do shit except checking for light schedule
-        if (self.current_visit.class_ == "S" or self.current_visit.reinforcement == False):
-            self.log.debug("Silence Perching")
-            while True:
-                if (self.validate_deperching() == True or self.check_session_schedule() == False):
-                    if self.check_session_schedule() == False:
-                        self.current_visit.perch_end = dt.datetime.now()
-                    return
-        ## if the current class is not silence, prep and play stimuli
-        else:
-            self.prep_stimuli()
-            self.play_stimuli()
-
-        while True:
-            ## if deperching has been detected, or light schedule expires, quit this function
-            if (self.validate_deperching() == True or self.check_session_schedule() == False):
-                if self.check_session_schedule() == False:
-                    self.current_visit.perch_end = dt.datetime.now()
-                self.stop_stimuli()
-                return
-            ## else, play audio until its length runs out,
-            else:
-                elapsed_time = (dt.datetime.now() - self.stimulus_event.time).total_seconds()
-                if elapsed_time < self.stimulus_event.duration:
-                    continue
-                # when it does, give an inter_stim_interval, and recurse on the function
-                else:
-                    if elapsed_time < (self.stimulus_event.duration + self.parameters['inter_stim_interval']):
-                        continue
-                    ## when inter_stim_interval runs out, stop stimuli and go back to the stimulus_shuffle
-                    else:
-                        self.log.debug("Stimuli finished and inter_stim_interval has passed. ")
-                        self.stop_stimuli()
-                        ## find another clip to play
-                        self.prep_stimuli()
-                        self.play_stimuli()
-
-    def perch_playlist(self):
-        """
-        depending on the perch and the perch_sequence, find the correct list
-        """
-        if self.current_perch_stim_class() == "A":
-            self.log.debug("Perch stim class A")
-            return self.parameters['stims_A']
-        if self.current_perch_stim_class() == "B":
-            self.log.debug("Perch stim class B")
-            return self.parameters['stims_B']
-
-    def prep_stimuli(self):
-        """
-        Prep stimuli and generate a stimulus event
-        """
-        ## randomly shuffle from the current perch_playlist
-        stim_file = random.sample(self.perch_playlist().items(), k = 1)[0][1]
-        self.log.debug(stim_file)
-        stim = utils.auditory_stim_from_wav(stim_file)
-
-        self.stimulus_event = utils.Event(
-            time = dt.datetime.now(),
-            duration = stim.duration,
-            file_origin = stim.file_origin,
-        )
-
-        self.log.debug("Queuing stimulus %s" % stim.file_origin)
-        self.panel.speaker.queue(self.stimulus_event.file_origin)
-
-    def play_stimuli(self):
-        """
-        Play stimuli through current_perch speaker
-        """
-
-        self.log.debug("Playing %s" % (self.stimulus_event.file_origin))
-        ## trigger speaker
-        self.panel.speaker.play()
-
-    def stop_stimuli(self):
-        """
-        Stop stimuli, record event, and clear out event
-        """
-        self.log.debug("Stop stimuli and flush stimulus event.")
-        self.panel.speaker.stop()
-        self.current_visit.stimuli.append(self.stimulus_event.file_origin)
-        self.current_visit.events.append(self.stimulus_event)
-        self.stimulus_event = None
-
-    def end_visit(self):
-        """
-        End visit and write data of current visit to csv
-        """
-        self.log.debug("Ending visit and record end time.")
-        try:
-            self.current_visit.perch_dur = (self.current_visit.perch_end - self.current_visit.perch_strt).total_seconds()
-        except:
-            self.current_visit.perch_dur = 0
-        self.current_visit.valid = (self.current_visit.perch_dur >= self.parameters['min_perch_time'])
-        self.save_visit(self.current_visit)
-
-    def save_visit(self, visit):
-        """
-        write visit results to CSV
-        """
-
-        self.log.debug("Writing data to %s" % (self.data_csv))
-        visit_dict = {}
-        for field in self.fields_to_save:
-            try:
-                visit_dict[field] = getattr(visit,field)
-            except AttributeError:
-                visit_dict[field] = visit.annotations[field] ## it might be in annotations for some reason
-
-        with open(self.data_csv, 'ab') as data_fh:
-            visitWriter = csv.DictWriter(data_fh, fieldnames = self.fields_to_save, extrasaction = 'ignore')
-            visitWriter.writerow(visit_dict)
-
-    def reset_perches(self):
-        """
-        Reset perches
-        """
-
-        self.current_perch = {'IR': None, 'IRName': None, 'speaker': None}
-        self.stimulus_event = None
-        self.current_visit = None
-
-    def check_session_schedule(self):
-        """
-        Check if perches should be open
-
-        returns
-        -------
-        bool
-            True, if sessions should be running
-        """
-        return utils.check_time(self.parameters['light_schedule'])
 
     def session_main(self):
         """
-        Inside session_main, maintain a loop that controls paradigm behavior
+        Inside session_pre, maintain a loop that controls *day* behavior.
         """
 
-        ## while the session is in daylight
-        while self.check_session_schedule() == True:
+        while True:
+
             '''
             Try to open all perches. The program loops in open_all_perches
             until a valid perching has been detected.
             '''
             self.open_all_perches()
-
-            '''
-            Once perching has been detected, switch speaker to the current
-            perch, and start stimuli shuffle. The program loops in stimulus_shuffle()
-            until a valid deperching has been detected()
-            '''
-            if (self.current_perch['IR'] != None):
-                if (self.current_perch['IR'].status() == True):
-                    self.switch_speaker()
-                    self.stimulus_shuffle()
-
-            '''
-            Once deperched, end visit and reset
-            '''
-            self.end_visit()
-            self.reset_perches()
-
-        ## while it is nighttime, enter alternative nighttime loop
-        while self.check_session_schedule() == False:
-            '''
-            Try to open all perches. The program loops in open_all_perches
-            until a valid perching has been detected.
-            '''
-            self.open_all_perches()
-
-
-
-
-    def session_post(self):
-        self.reinforcement_counter = {'L': None, 'R': None, 'C': None} ## flush reinforcement
-        self.log.info('Flushing reinforcement counter. ')
-        self.log.info('Paradigm is closed. Proceed to Sleep. ')
-        return None
